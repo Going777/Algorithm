6623f07a1e9f7a95b837a0becfce5f06aecef681
Going777 <109488657+Going777@users.noreply.github.com>
2022-07-25 23:25:03 +0900

    Update BOJ_13458_시험 감독.py

--- BOJ_13458_시험 감독.py
+++ BOJ_13458_시험 감독.py
@@ -2,12 +2,12 @@ T = int(input())
 class_list = list(map(int, input().split()))
 B, C = map(int, input().split())
 
-result = 0
+result = T # 총감독관은 클래스마다 한 명씩 존재
 for cl in class_list:
-    result += 1 # 총감독관은 무조건 한 명
-    remain = cl - B # 총감독관이 감독할 수 있는 학생 제외 응시생
-    result += remain // C # 부감독관 필요 숫자
-    if remain % C != 0: # 나누어 떨어지지 않고 남으면
-        result += 1 # 부감독관 한 명 추가
+    cl -=  B # 총감독관이 감독할 수 있는 학생 제외 응시생
+    if cl > 0:
+        result += (cl // C) # 부감독관 필요 숫자
+        if cl % C: # 나누어 떨어지지 않고 남으면
+            result += 1 # 부감독관 한 명 추가
         
 print(result)
