c740e11da3a9e5df99dde3f85286c0d87cb7d517
JW <wjddnjs0528@gmail.com>
2022-08-18 22:33:33 +0900

    220818

--- BOJ/Silver/1158_요세푸스 문제.py
+++ BOJ/Silver/1158_요세푸스 문제.py
@@ -1,14 +1,14 @@
 import sys
 N, K = map(int, sys.stdin.readline().split())
-arr = list(range(1, N+1))
-result = ["<"]
-i = 0
-while True:
-    if len(result) >= len(arr):
-        break
-    if i == K-1:
-        result.append(arr[i])
-    i += 1
-    K += K
-    if K >= len(arr):
-        K = K % len(arr)
+arr = list(range(1, N+1))           # 기준 배열
+result = []                         # 정답 배열
+
+idx = 0
+while arr:
+    idx += K-1                      # 인덱스 값이 K-1의 배수인곳 제거
+    if idx >= len(arr):             # 인덱스 값이 배열 크기 보다 크거나 같아진다면,
+        idx %= len(arr)             # 해당 인덱스 값은 그 값을 배열 크기로 나눈 나머지 값으로 설정
+    result.append(arr.pop(idx))     # 정답 배열에 인덱스에 해당하는 값 넣어주고, 기준 배열에서는 삭제
+
+print(f"<{', '.join(map(str,result))}>")
+
