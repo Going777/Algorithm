80700b142c60c8158317c7c12d00855ff0142ba9
Jungwon Kim <wjddnjs0528@gmail.com>
2024-12-09 14:51:32 +0900

    solve: 1012

---
+++ BOJ/Silver/1012_유기농배추/1012_유기농 배추.py
@@ -0,0 +1,98 @@
+# from collections import deque
+# import sys
+# input = sys.stdin.readline
+
+# di = [-1,1,0,0] ; dj = [0,0,-1,1]
+# def bfs(i, j):
+#     q = deque()
+#     q.append([i, j])
+#     visited[i][j] = 1
+#     arr[i][j] = 2
+
+#     while q:
+#         i, j = q.popleft()
+
+
+#         for k in range(4):
+#             ni = i + di[k] ; nj = j + dj[k]
+#             if 0 <= ni < N and 0 <= nj < M and not visited[ni][nj] and arr[ni][nj] == 1:
+#                 q.append([ni, nj])
+#                 visited[ni][nj] = 1
+#                 arr[ni][nj] = 2
+
+# T = int(input())    # 테스트 케이스 개수
+# for _ in range(T):
+#     N, M, K = map(int, input().split())     # N: 행 개수 / M: 열 개수 / K: 배추개수
+#     arr = [[0]*M for _ in range(N)]
+#     for _ in range(K):
+#         a, b = map(int, input().split())
+#         arr[a][b] = 1
+
+#     cnt = 0
+#     visited = [[0]*M for _ in range(N)]
+#     for i in range(N):
+#         for j in range(M):
+#             if arr[i][j] == 1:
+#                 cnt += 1
+#                 bfs(i, j)
+#     print(cnt)
+
+# '''
+# 2
+# 10 8 17
+# 0 0
+# 1 0
+# 1 1
+# 4 2
+# 4 3
+# 4 5
+# 2 4
+# 3 4
+# 7 4
+# 8 4
+# 9 4
+# 7 5
+# 8 5
+# 9 5
+# 7 6
+# 8 6
+# 9 6
+# 10 10 1
+# 5 5
+# '''
+
+import sys
+sys.setrecursionlimit(10 ** 6)
+
+t = int(input())
+di = [-1,1,0,0]
+dj = [0,0,-1,1]
+def dfs(i, j):
+    visited[i][j] = 1
+    
+    for k in range(4):
+        ni = i + di[k]
+        nj = j + dj[k]
+        if (0 <= ni < N and 0 <= nj < M and arr[ni][nj] == 1 and not visited[ni][nj]):
+            dfs(ni, nj)
+
+for _ in range(t):
+    N, M, K = map(int, input().split())
+    arr = [[0]*M for _ in range(N)]
+    visited = [[0]*M for _ in range(N)]
+    cnt = 0
+    for _ in range(K):
+        i, j = map(int, input().split())
+        arr[i][j] = 1
+    for i in range(N):
+        for j in range(M):
+            if (arr[i][j] == 1 and not visited[i][j]):
+                cnt += 1
+                dfs(i, j)
+
+    print(cnt)
+
+
+
+
+
