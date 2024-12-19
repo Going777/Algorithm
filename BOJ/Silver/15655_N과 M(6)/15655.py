dff9a58e2267d8ffc22af0aab09ff69ef13a30e9
Jungwon Kim <wjddnjs0528@gmail.com>
2024-12-19 17:41:47 +0900

    solve: 15655

---
+++ BOJ/Silver/15655_N과 M(6)/15655.py
@@ -0,0 +1,21 @@
+import sys
+sys.setrecursionlimit(10 ** 6)
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+lst = sorted(list(map(int, input().split())))
+visited = [0] * N 
+
+def dfs(n, numLst):
+    if n == M:
+        print(*numLst)
+        return
+    
+    for i in range(N):
+        targetValue = numLst[-1] if numLst else 0
+        if not visited[i] and targetValue < lst[i]:
+            visited[i] = 1
+            dfs(n + 1, numLst + [lst[i]])
+            visited[i] = 0
+
+dfs(0, [])
